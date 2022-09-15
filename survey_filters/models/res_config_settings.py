fd7c0a31bbde56a01b0897616c70a439d69966ac
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-09-15 10:17:52 +0300

    Modified module

---
+++ survey_filters/models/res_config_settings.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    use_user_filter = fields.Boolean(
+        string="Survey: use the user filter in the survey result view",
+        config_parameter="survey.filter.user",
+    )
+
+    use_date_filter = fields.Boolean(
+        string="Survey: use the date filter in the survey result view",
+        config_parameter="survey.filter.date",
+    )
+
+    module_society_event_core = fields.Boolean(string='Use event')
+
+    use_event_filter = fields.Boolean(
+        string="Survey: use the event filter in the survey result view",
+        config_parameter="survey.filter.event",
+    )
--- survey_filters/static/src/js/survey_filter.js
+++ survey_filters/static/src/js/survey_filter.js
@@ -16,5 +16,52 @@ odoo.define("survey.survey_page_statistics_inner", function () {
                 window.location.href = path;
             }
         });
+
+        $("#select_event").on("change", function () {
+            var val = $(this).val();
+            var path = window.location.pathname;
+
+            if (path.indexOf("/event") >= 0) {
+                path = window.location.pathname.split("/event")[0];
+            }
+
+            if (val) {
+                window.location.href = path + "/event/" + val;
+            } else {
+                window.location.href = path;
+            }
+        });
+
+        var path = window.location.pathname
+        if (path.indexOf("/date") >= 0) {
+            var date_current = path.split("/date/")[1];
+            var newDateTime = moment(date_current, "DD.MM.YYYY").toDate();
+            $('#datetimepicker-filter-date').datetimepicker({
+                format : 'DD.MM.Y',
+                locale: moment.locale(),
+                defaultDate: newDateTime,
+            });
+        }else {
+            var dateNow = new Date();
+            $('#datetimepicker-filter-date').datetimepicker({
+                format : 'DD.MM.Y',
+                locale: moment.locale(),
+                defaultDate: dateNow,
+            });
+        }
+
+        $("#datetimepicker-filter-date").on("change.datetimepicker", function() {
+            var date = $(this).find('.datetimepicker-input').val();
+            var path = window.location.pathname
+            if (path.indexOf("/date") >= 0) {
+                path = window.location.pathname.split("/date")[0];
+            }
+
+            if (date) {
+                window.location.href = path + "/date/" + date;
+            } else {
+                window.location.href = path;
+            }
+        });
     });
 });
