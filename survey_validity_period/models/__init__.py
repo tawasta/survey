37faa5f18f7182352d95e26eb3c2e0a5546c0b9e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-09-21 15:30:37 +0300

    Created a new module

---
+++ survey_validity_period/models/__init__.py
@@ -0,0 +1,3 @@
+from . import survey_survey
+from . import survey_user_stage
+from . import user_input
---
+++ survey_validity_period/models/survey_survey.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+
+class SurveySurvey(models.Model):
+
+    _inherit = "survey.survey"
+
+    qualification_period = fields.Boolean(string="Specify the period of qualification", default=False)
+
+    interval_nbr = fields.Integer('Interval', default=1)
+
+    interval_unit = fields.Selection([
+        ('days', 'Days')],
+        string='Unit', default='days',)
