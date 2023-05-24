db884237c80f74630fc4d916f79572975544dda0
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-05-24 16:16:21 +0300

    Modified module

--- survey_filters/controllers/main.py
+++ survey_filters/controllers/main.py
@@ -175,18 +175,64 @@ class SurveyFilter(Survey):
 
         return request.render("survey.survey_page_statistics", template_values)
 
-    def _extract_survey_data(
-        self, survey, user_id, event_id, select_date, date_end, search, post
-    ):
+
+    def _get_user_input_domain(self, survey, line_filter_domain, **post):
+        user_input_domain = ['&', ('test_entry', '=', False), ('survey_id', '=', survey.id)]
+        if line_filter_domain:
+            matching_line_ids = request.env['survey.user_input.line'].sudo().search(line_filter_domain).ids
+            logging.info(matching_line_ids);
+            user_input_domain = expression.AND([
+                [('user_input_line_ids', 'in', matching_line_ids)],
+                user_input_domain
+            ])
+        if post.get('finished'):
+            user_input_domain = expression.AND([[('state', '=', 'done')], user_input_domain])
+        else:
+            user_input_domain = expression.AND([[('state', '!=', 'new')], user_input_domain])
+        return user_input_domain
+
+
+    def _extract_survey_data(self, survey, user_id, event_id, select_date, date_end, search, post):
         search_filters = []
-        line_filter_domain = []
-        line_choices = []
+        line_filter_domain, line_choices = [], []
+        for data in post.get('filters', '').split('|'):
+            try:
+                row_id, answer_id = (int(item) for item in data.split(','))
+            except:
+                pass
+            else:
+                if row_id and answer_id:
+                    line_filter_domain = expression.AND([
+                        ['&', ('matrix_row_id', '=', row_id), ('suggested_answer_id', '=', answer_id)],
+                        line_filter_domain
+                    ])
+                    answers = request.env['survey.question.answer'].browse([row_id, answer_id])
+                elif answer_id:
+                    line_choices.append(answer_id)
+                    answers = request.env['survey.question.answer'].browse([answer_id])
+                if answer_id:
+                    question_id = answers[0].matrix_question_id or answers[0].question_id
+                    search_filters.append({
+                        'question': question_id.title,
+                        'answers': '%s%s' % (answers[0].value, ': %s' % answers[1].value if len(answers) > 1 else '')
+                    })
+        if line_choices:
+            #line_filter_domain = expression.AND([[('suggested_answer_id', '=', line_choices)], line_filter_domain])
+            for lc in line_choices:
+                line_filter_domain += [('user_input_line_ids.suggested_answer_id', '=', lc)]
+        line_filter_domain += [('test_entry', '=', False)]
+        line_filter_domain += [('survey_id', '=', survey.id)]
+        if post.get('finished'):
+            line_filter_domain += [('state', '=', 'done')]
+        else:
+            line_filter_domain += [('state', '!=', 'new')]
+
         if search:
-            line_filter_domain += [("user_input_id.event_id.name", "ilike", search)]
+            line_filter_domain += [("event_id.name", "ilike", search)]
         if event_id:
-            line_filter_domain += [("user_input_id.event_id", "=", event_id)]
+            line_filter_domain += [("event_id", "=", event_id)]
         if user_id:
-            line_filter_domain += [("user_input_id.partner_id", "=", user_id)]
+            line_filter_domain += [("partner_id", "=", user_id)]
 
         if select_date and not date_end:
             select_date_obj = datetime.strptime(select_date, "%d.%m.%Y")
@@ -194,8 +240,8 @@ class SurveyFilter(Survey):
                 hours=23, minutes=59, seconds=59
             )
             line_filter_domain += [
-                ("user_input_id.create_date", ">=", select_date_obj),
-                ("user_input_id.create_date", "<=", select_date_end_obj),
+                ("create_date", ">=", select_date_obj),
+                ("create_date", "<=", select_date_end_obj),
             ]
         if select_date and date_end:
             select_date_start_obj = datetime.strptime(select_date, "%d.%m.%Y")
@@ -204,67 +250,108 @@ class SurveyFilter(Survey):
                 hours=23, minutes=59, seconds=59
             )
             line_filter_domain += [
-                ("user_input_id.create_date", ">=", select_date_start_obj),
-                ("user_input_id.create_date", "<=", date_end_obj),
+                ("create_date", ">=", select_date_start_obj),
+                ("create_date", "<=", date_end_obj),
             ]
-        if (
-            not user_id
-            and not select_date
-            and not event_id
-            and not search
-            and not date_end
-        ):
-            line_filter_domain, line_choices = [], []
-        for data in post.get("filters", "").split("|"):
-            try:
-                row_id, answer_id = (int(item) for item in data.split(","))
-            except Exception:
-                pass
-            else:
-                if row_id and answer_id:
-                    line_filter_domain = expression.AND(
-                        [
-                            [
-                                "&",
-                                ("matrix_row_id", "=", row_id),
-                                ("suggested_answer_id", "=", answer_id),
-                            ],
-                            line_filter_domain,
-                        ]
-                    )
-                    answers = request.env["survey.question.answer"].browse(
-                        [row_id, answer_id]
-                    )
-                elif answer_id:
-                    line_choices.append(answer_id)
-                    answers = request.env["survey.question.answer"].browse([answer_id])
-                if answer_id:
-                    question_id = (
-                        answers[0].matrix_question_id or answers[0].question_id
-                    )
-                    search_filters.append(
-                        {
-                            "question": question_id.title,
-                            "answers": "%s%s"
-                            % (
-                                answers[0].value,
-                                ": %s" % answers[1].value if len(answers) > 1 else "",
-                            ),
-                        }
-                    )
-        if line_choices:
-            line_filter_domain = expression.AND(
-                [[("suggested_answer_id", "in", line_choices)], line_filter_domain]
-            )
+        
+        user_input_lines = request.env["survey.user_input"].sudo().search(line_filter_domain).mapped('user_input_line_ids')
+        logging.info(user_input_lines);
+        #user_input_domain = self._get_user_input_domain(survey, line_filter_domain, **post)
 
-        user_input_domain = self._get_user_input_domain(
-            survey, line_filter_domain, **post
-        )
-        user_input_lines = (
-            request.env["survey.user_input"]
-            .sudo()
-            .search(user_input_domain)
-            .mapped("user_input_line_ids")
-        )
+        #user_input_lines = request.env['survey.user_input'].sudo().search(user_input_domain).mapped('user_input_line_ids')
 
         return user_input_lines, search_filters
+
+    # def _extract_survey_data(
+    #     self, survey, user_id, event_id, select_date, date_end, search, post
+    # ):
+    #     search_filters = []
+    #     line_filter_domain = []
+    #     line_choices = []
+    #     if search:
+    #         line_filter_domain += [("user_input_id.event_id.name", "ilike", search)]
+    #     if event_id:
+    #         line_filter_domain += [("user_input_id.event_id", "=", event_id)]
+    #     if user_id:
+    #         line_filter_domain += [("user_input_id.partner_id", "=", user_id)]
+
+    #     if select_date and not date_end:
+    #         select_date_obj = datetime.strptime(select_date, "%d.%m.%Y")
+    #         select_date_end_obj = select_date_obj + timedelta(
+    #             hours=23, minutes=59, seconds=59
+    #         )
+    #         line_filter_domain += [
+    #             ("user_input_id.create_date", ">=", select_date_obj),
+    #             ("user_input_id.create_date", "<=", select_date_end_obj),
+    #         ]
+    #     if select_date and date_end:
+    #         select_date_start_obj = datetime.strptime(select_date, "%d.%m.%Y")
+    #         select_date_end_obj = datetime.strptime(date_end, "%d.%m.%Y")
+    #         date_end_obj = select_date_end_obj + timedelta(
+    #             hours=23, minutes=59, seconds=59
+    #         )
+    #         line_filter_domain += [
+    #             ("user_input_id.create_date", ">=", select_date_start_obj),
+    #             ("user_input_id.create_date", "<=", date_end_obj),
+    #         ]
+    #     if (
+    #         not user_id
+    #         and not select_date
+    #         and not event_id
+    #         and not search
+    #         and not date_end
+    #     ):
+    #         line_filter_domain, line_choices = [], []
+    #     for data in post.get("filters", "").split("|"):
+    #         try:
+    #             row_id, answer_id = (int(item) for item in data.split(","))
+    #         except Exception:
+    #             pass
+    #         else:
+    #             if row_id and answer_id:
+    #                 line_filter_domain = expression.AND(
+    #                     [
+    #                         [
+    #                             "&",
+    #                             ("matrix_row_id", "=", row_id),
+    #                             ("suggested_answer_id", "=", answer_id),
+    #                         ],
+    #                         line_filter_domain,
+    #                     ]
+    #                 )
+    #                 answers = request.env["survey.question.answer"].browse(
+    #                     [row_id, answer_id]
+    #                 )
+    #             elif answer_id:
+    #                 line_choices.append(answer_id)
+    #                 answers = request.env["survey.question.answer"].browse([answer_id])
+    #             if answer_id:
+    #                 question_id = (
+    #                     answers[0].matrix_question_id or answers[0].question_id
+    #                 )
+    #                 search_filters.append(
+    #                     {
+    #                         "question": question_id.title,
+    #                         "answers": "%s%s"
+    #                         % (
+    #                             answers[0].value,
+    #                             ": %s" % answers[1].value if len(answers) > 1 else "",
+    #                         ),
+    #                     }
+    #                 )
+    #     if line_choices:
+    #         line_filter_domain = expression.AND(
+    #             [[("suggested_answer_id", "in", line_choices)], line_filter_domain]
+    #         )
+
+    #     user_input_domain = self._get_user_input_domain(
+    #         survey, line_filter_domain, **post
+    #     )
+    #     user_input_lines = (
+    #         request.env["survey.user_input"]
+    #         .sudo()
+    #         .search(user_input_domain)
+    #         .mapped("user_input_line_ids")
+    #     )
+
+    #     return user_input_lines, search_filters
