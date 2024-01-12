db4f1542f6774ddae63c038d6088b8c252d3c1f8
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-01-12 12:25:25 +0200

    Modified js function

--- survey_filters/static/src/js/survey_filter.js
+++ survey_filters/static/src/js/survey_filter.js
@@ -73,18 +73,46 @@ odoo.define("survey.survey_page_statistics_inner", function () {
         }
 
         $("#datetimepicker-filter-date").on("hide.datetimepicker", function () {
-            var date = $(this).find(".datetimepicker-input").val();
-            var path = window.location.pathname;
-            if (path.indexOf("/date_start") >= 0) {
-                path = window.location.pathname.split("/date_start")[0];
-            }
+            var newStartDate = $(this).find(".datetimepicker-input").val();
+            var basePath = window.location.pathname.split("/date_start")[0].split("/date_end")[0];
+            var endDatePath = basePath + (window.location.pathname.indexOf("/date_end") >= 0 ? window.location.pathname.split("/date_end")[1] : "");
 
-            if (date) {
-                window.location.href = path + "/date_start/" + date;
+            if (newStartDate) {
+                var newPath = basePath + "/date_start/" + newStartDate;
+                // Check if end_date exists and compare dates
+                if (endDatePath) {
+                    //console.log("=====TANNE MENENEE=====");
+                    var endDate = moment(window.location.pathname.split("/date_end/")[1], "DD.MM.YYYY");
+                    var startDate = moment(newStartDate, "DD.MM.YYYY");
+
+                    //console.log(startDate);
+                    //console.log(endDate);
+                    if (startDate.isBefore(endDate)) {
+                        newPath += "/date_end/" + window.location.pathname.split("/date_end/")[1];
+                    }
+                }
+                window.location.href = newPath;
+                //console.log(newPath);
             } else {
-                window.location.href = path;
+                window.location.href = basePath;
+                //console.log("basePath");
             }
         });
+
+        // $("#datetimepicker-filter-date").on("hide.datetimepicker", function () {
+        //     var date = $(this).find(".datetimepicker-input").val();
+        //     console.log(date);
+        //     var path = window.location.pathname;
+        //     if (path.indexOf("/date_start") >= 0) {
+        //         path = window.location.pathname.split("/date_start")[0];
+        //     }
+
+        //     if (date) {
+        //         window.location.href = path + "/date_start/" + date;
+        //     } else {
+        //         window.location.href = path;
+        //     }
+        // });
         if (path.indexOf("/date_end") >= 0) {
             var date_current = path.split("/date_end/")[1];
             var newDateTime = moment(date_current, "DD.MM.YYYY").toDate();
