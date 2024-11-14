080b57c1317d0e3116a16637445227bc42866a81
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-11-14 14:08:23 +0200

    [IMP] - modified readme

--- survey_question_ssn/models/survey_user_input_line.py
+++ survey_question_ssn/models/survey_user_input_line.py
@@ -1,42 +1,54 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+import textwrap
 
 
 class SurveyUserInputLine(models.Model):
-
     _inherit = "survey.user_input.line"
 
-    value_ssn = fields.Char('SSN answer')
+    value_ssn = fields.Char("SSN answer")
 
     answer_type = fields.Selection(
-        selection_add=[('ssn', 'SSN')],
+        selection_add=[("ssn", "SSN")],
     )
 
     @api.depends(
-        'answer_type', 'value_text_box', 'value_numerical_box',
-        'value_char_box', 'value_date', 'value_datetime',
-        'value_ssn', 'suggested_answer_id.value', 'matrix_row_id.value',
+        "answer_type",
+        "value_text_box",
+        "value_numerical_box",
+        "value_char_box",
+        "value_date",
+        "value_datetime",
+        "value_ssn",
+        "suggested_answer_id.value",
+        "matrix_row_id.value",
     )
     def _compute_display_name(self):
         for line in self:
-            user_in_group = self.env.user.has_group('survey_question_ssn.group_social_security_number')
-            if line.answer_type == 'char_box':
+            user_in_group = self.env.user.has_group(
+                "survey_question_ssn.group_social_security_number"
+            )
+            if line.answer_type == "char_box":
                 line.display_name = line.value_char_box
-            elif line.answer_type == 'text_box' and line.value_text_box:
-                line.display_name = textwrap.shorten(line.value_text_box, width=50, placeholder=" [...]")
-            elif line.answer_type == 'numerical_box':
+            elif line.answer_type == "text_box" and line.value_text_box:
+                line.display_name = textwrap.shorten(
+                    line.value_text_box, width=50, placeholder=" [...]"
+                )
+            elif line.answer_type == "numerical_box":
                 line.display_name = line.value_numerical_box
-            elif line.answer_type == 'date':
+            elif line.answer_type == "date":
                 line.display_name = fields.Date.to_string(line.value_date)
-            elif line.answer_type == 'datetime':
+            elif line.answer_type == "datetime":
                 line.display_name = fields.Datetime.to_string(line.value_datetime)
-            elif line.answer_type == 'suggestion':
+            elif line.answer_type == "suggestion":
                 if line.matrix_row_id:
-                    line.display_name = f'{line.suggested_answer_id.value}: {line.matrix_row_id.value}'
+                    line.display_name = (
+                        f"{line.suggested_answer_id.value}: {line.matrix_row_id.value}"
+                    )
                 else:
                     line.display_name = line.suggested_answer_id.value
-            elif line.answer_type == 'ssn':
+            elif line.answer_type == "ssn":
                 line.display_name = line.value_ssn if user_in_group else ""
 
-            if line.answer_type != 'ssn' and not line.display_name:
-                line.display_name = _('Skipped')
+            if line.answer_type != "ssn" and not line.display_name:
+                line.display_name = _("Skipped")
--- survey_question_ssn/static/src/js/survey.esm.js
+++ survey_question_ssn/static/src/js/survey.esm.js
@@ -1,14 +1,14 @@
 /** @odoo-module */
 
-import SurveyFormWidget from '@survey/js/survey_form';
+import SurveyFormWidget from "@survey/js/survey_form";
 
 SurveyFormWidget.include({
     _prepareSubmitValues(formData, params) {
         this._super(...arguments); // Kutsutaan alkuperäistä funktiota
 
-        this.$('[data-question-type]').each(function () {
-            if ($(this).data('questionType') === 'ssn') {
-                params[$(this).attr('name')] = $(this).val();
+        this.$("[data-question-type]").each(function () {
+            if ($(this).data("questionType") === "ssn") {
+                params[$(this).attr("name")] = $(this).val();
             }
         });
     },
