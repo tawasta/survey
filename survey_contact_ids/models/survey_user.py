2cc94993b9ce2b10c0e6f847d25be8715c5c654c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-04-10 08:21:20 +0300

    [MIG] - modified module to 17 version

--- survey_contact_ids/models/survey_user.py
+++ survey_contact_ids/models/survey_user.py
@@ -99,12 +99,12 @@ class SurveyUserInput(models.Model):
                 }
             )
 
-    def save_lines(self, question, answer, comment=None):
+    def _save_lines(self, question, answer, comment=None, overwrite_existing=False):
         """Save answers to questions, depending on question type
         If an answer already exists for question and user_input_id, it will be
         overwritten (or deleted for 'choice' questions) (in order to maintain data consistency).
         """
-        res = super(SurveyUserInput, self).save_lines(question, answer, comment)
+        res = super(SurveyUserInput, self)._save_lines(question, answer, comment, overwrite_existing)
         if (
             question.question_type == "char_box"
             and question.save_as_contact_name
