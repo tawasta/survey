62278a58eb64b4768c245f94d503301b759a8643
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-04-17 13:34:08 +0300

    [IMP] - created new module

---
+++ survey_registry_portal/README.rst
@@ -0,0 +1,55 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+        :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+        :alt: License: AGPL-3
+
+======================
+Survey Registry Portal
+======================
+
+* Adds **"Survey Registry"** section to the portal (`/surveys/registry`)
+* Portal users can view and search through completed surveys
+* Surveys can be searched by respondent name and survey title
+* Sorting options:
+  * Newest submissions first
+  * Respondent name
+  * Survey title
+* Each survey submission has its own detailed page including:
+  * Respondent name
+  * Submission date
+  * Survey title
+  * Detailed answers provided by the respondent
+* Integrated neatly within the default Odoo portal layout
+
+Configuration
+=============
+
+- No additional configuration is required  
+- Ensure `survey.user_input` records have a `partner_id` assigned for visibility
+
+Usage
+=====
+
+- Go to **Survey Registry** (`/surveys/registry`)  
+- Browse or search survey submissions  
+- Select a submission to view detailed answers  
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Valtteri Lattu <valtteri.lattu@futural.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+        :alt: Oy Tawasta OS Technologies Ltd.
+        :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ survey_registry_portal/controllers/main.py
@@ -0,0 +1,96 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http, _
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.osv.expression import OR
+
+
+class SurveyRegistryPortal(CustomerPortal):
+
+    # def _prepare_home_portal_values(self, counters):
+    #     values = super()._prepare_home_portal_values(counters)
+    #     if 'survey_registry_count' in counters:
+    #         values['survey_registry_count'] = request.env['survey.user_input'].search_count([('state', '=', 'done')]) \
+    #             if request.env['survey.user_input'].check_access_rights('read', raise_exception=False) else 0
+    #     return values
+
+    def _get_survey_registry_domain(self):
+        return [('state', '=', 'done')]
+
+    def _get_survey_registry_sortings(self):
+        return {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Respondent'), 'order': 'partner_id'},
+            'survey': {'label': _('Survey'), 'order': 'survey_id'},
+        }
+
+    def _get_survey_registry_inputs(self):
+        return {
+            'all': {'label': _('Search in All'), 'input': 'all'},
+            'name': {'label': _('Respondent'), 'input': 'name'},
+            'survey': {'label': _('Survey'), 'input': 'survey'},
+        }
+
+    def _get_survey_registry_search_domain(self, search_in, search):
+        domain = []
+        if search_in in ('name', 'all'):
+            domain.append([('partner_id.name', 'ilike', search)])
+        if search_in in ('survey', 'all'):
+            domain.append([('survey_id.title', 'ilike', search)])
+        return OR(domain)
+
+    def _prepare_survey_registry_values(self, page, search=None, search_in='all', sortby=None, **kwargs):
+        SurveyInput = request.env['survey.user_input'].sudo()
+        values = self._prepare_portal_layout_values()
+
+        domain = self._get_survey_registry_domain()
+        sortings = self._get_survey_registry_sortings()
+        inputs = self._get_survey_registry_inputs()
+
+        if not sortby or sortby not in sortings:
+            sortby = 'date'
+        order = sortings[sortby]['order']
+
+        if search:
+            domain = domain + self._get_survey_registry_search_domain(search_in, search)
+
+        total = SurveyInput.search_count(domain)
+
+        pager = portal_pager(
+            url='/surveys/registry',
+            url_args={'search': search, 'search_in': search_in, 'sortby': sortby},
+            total=total,
+            page=page,
+            step=self._items_per_page
+        )
+
+        user_inputs = SurveyInput.search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
+
+        values.update({
+            'inputs': user_inputs,
+            'page_name': 'survey_registry',
+            'default_url': '/surveys/registry',
+            'pager': pager,
+            'search': search,
+            'search_in': search_in,
+            'sortby': sortby,
+            'searchbar_sortings': sortings,
+            'searchbar_inputs': inputs,
+        })
+        return values
+
+    @http.route(['/surveys/registry', '/surveys/registry/page/<int:page>'], type='http', auth='public', website=True)
+    def portal_survey_registry(self, page=1, search=None, search_in='all', sortby=None, **kw):
+        values = self._prepare_survey_registry_values(page, search=search, search_in=search_in, sortby=sortby)
+        return request.render("survey_registry_portal.survey_registry_page", values)
+
+    @http.route(['/surveys/registry/<int:input_id>'], type='http', auth='public', website=True)
+    def portal_survey_registry_detail(self, input_id, **kw):
+        survey_input = request.env['survey.user_input'].sudo().browse(input_id)
+        if not survey_input.exists() or survey_input.state != 'done':
+            return request.render("website.404")
+        return request.render("survey_registry_portal.survey_registry_detail", {
+            'input': survey_input,
+            'page_name': 'survey_registry_detail',
+        })
