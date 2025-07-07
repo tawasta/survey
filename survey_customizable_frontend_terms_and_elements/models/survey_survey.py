1bf2679d87cde356cf57c2f1e6befe838630c05a
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-07-07 11:42:59 +0300

    [IMP] - added fi po file

--- survey_customizable_frontend_terms_and_elements/models/survey_survey.py
+++ survey_customizable_frontend_terms_and_elements/models/survey_survey.py
@@ -4,4 +4,9 @@ from odoo import fields, models
 class SurveySurvey(models.Model):
     _inherit = "survey.survey"
 
-    answer_button_text = fields.Char(translate=True)
+    answer_button_text = fields.Char(
+        translate=True,
+        help="Enter a custom label for the 'Start Survey'"
+        " or 'Start Certification' button. "
+        "Leave empty to use the default text.",
+    )
