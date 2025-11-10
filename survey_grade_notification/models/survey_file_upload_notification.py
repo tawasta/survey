a6cb59314430a2c0de85821cef3faa262fd23db9
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-11-10 14:49:48 +0200

    [IMP] - added timezone

--- survey_grade_notification/models/survey_file_upload_notification.py
+++ survey_grade_notification/models/survey_file_upload_notification.py
@@ -1,4 +1,5 @@
 import logging
+import pytz
 
 from odoo import _, fields, models
 
@@ -33,6 +34,24 @@ class SurveyQuestion(models.Model):
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
+    # ---- TZ helpers ----
+    def _user_tz_name(self, user):
+        """Return user's timezone name or UTC fallback."""
+        tzname = (user.tz or "UTC").strip() or "UTC"
+        try:
+            pytz.timezone(tzname)
+            return tzname
+        except Exception:
+            return "UTC"
+
+    def _localize_for_user(self, dt, user):
+        """Return dt localized to the given user's timezone."""
+        if not dt:
+            return dt
+        tzname = self._user_tz_name(user)
+        # context_timestamp käsittelee Odoo-UTC-naive -> local
+        return fields.Datetime.context_timestamp(self.with_context(tz=tzname), dt)
+
     def _mark_done(self):
         super(SurveyUserInput, self)._mark_done()
         self._check_and_notify_low_responses()
@@ -45,62 +64,61 @@ class SurveyUserInput(models.Model):
         ).sudo()
         email_from = self.env.company.email
 
-        low_responses = []
         notifications = {}
 
         for user_input_line in self.user_input_line_ids:
             question = user_input_line.question_id
+
+            # MATRIX-kysymys: tarkista vain rivit, joissa check_min_value=True
             if (
                 question.question_type == "matrix"
                 and user_input_line.matrix_row_id.check_min_value
+                and question.min_acceptable_value
             ):
-                if (
-                    question.min_acceptable_value
-                    and user_input_line.suggested_answer_id.answer_score
-                    < question.min_acceptable_value
-                ):
-
-                    low_responses.append((question, user_input_line.answer_score))
+                # käytä line.answer_score jos sellainen on, muutoin suggested_answer_id.answer_score
+                score = (
+                    user_input_line.answer_score
+                    if user_input_line.answer_score is not None
+                    else getattr(user_input_line.suggested_answer_id, "answer_score", None)
+                )
+                if score is not None and score < question.min_acceptable_value:
                     for user in question.notification_recipients:
-                        if user not in notifications:
-                            notifications[user] = []
-                        notifications[user].append(
-                            (question, user_input_line.answer_score)
-                        )
-            if question.question_type != "matrix":
-                if (
-                    question.min_acceptable_value
-                    and user_input_line.answer_score < question.min_acceptable_value
-                ):
+                        notifications.setdefault(user, []).append((question, score))
 
-                    low_responses.append((question, user_input_line.answer_score))
+            # MUUT kysymystyypit
+            if question.question_type != "matrix" and question.min_acceptable_value:
+                score = user_input_line.answer_score
+                if score is not None and score < question.min_acceptable_value:
                     for user in question.notification_recipients:
-                        if user not in notifications:
-                            notifications[user] = []
-                        notifications[user].append(
-                            (question, user_input_line.answer_score)
-                        )
+                        notifications.setdefault(user, []).append((question, score))
 
         for user, questions in notifications.items():
-            logging.info(user)
+            if not user or not user.partner_id or not user.partner_id.email:
+                continue
             if email_template:
                 email_template.send_mail(
                     self.id,
                     email_values={
                         "email_to": user.partner_id.email,
                         "email_from": email_from,
-                        "body_html": self._generate_low_response_email_body(questions),
+                        # IMPORTANT: generoidaan body vastaanottajan omaan aikavyöhykkeeseen
+                        "body_html": self._generate_low_response_email_body(
+                            questions, user
+                        ),
                     },
                     notif_layout="mail.mail_notification_light",
                 )
 
     # flake8: noqa: C901
-    def _generate_low_response_email_body(self, low_responses):
-        """Generate an email body with details about the low responses."""
+    def _generate_low_response_email_body(self, low_responses, recipient_user):
+        """Generate an email body with details about the low responses for a given recipient (localized time)."""
+        # Localisoi luontiaika vastaanottajan aikavyöhykkeelle
+        local_dt = self._localize_for_user(self.create_date, recipient_user)
+        # Varmista, että meillä on strftime-kelpoinen arvo
+        time_str = local_dt.strftime("%d.%m.%Y %H:%M") if local_dt else "-"
+
         message = _("<p><b>Low feedback score given.</b></p>")
-        message += _(
-            f"<p><b>Time:</b> {self.create_date.strftime('%d.%m.%Y %H:%M')}</p>"
-        )
+        message += _(f"<p><b>Time:</b> {time_str}</p>")
         if self.event_id:
             message += _(
                 f"<p><b>Event:</b> {self.event_id.name if self.event_id else 'Not available'}</p>"
