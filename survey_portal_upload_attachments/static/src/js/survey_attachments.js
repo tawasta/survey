c4f79ac20dbf890215aa065953bb5d31586b0558
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-05-19 09:39:38 +0300

    Pre-commit fixes

--- survey_portal_upload_attachments/static/src/js/survey_attachments.js
+++ survey_portal_upload_attachments/static/src/js/survey_attachments.js
@@ -91,9 +91,8 @@ odoo.define("survey_portal_upload_attachments.survey_attachments", function (req
                                                 data: data,
                                             });
                                         });
-                                    self.file_value[
-                                        $(file_node).data("name")
-                                    ] = files_list;
+                                    self.file_value[$(file_node).data("name")] =
+                                        files_list;
                                     self.file_value[
                                         $(file_node).data("name")
                                     ].is_answer_update = true;
